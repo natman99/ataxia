ca61b97b2432ec9dee500cb58a815933da4a64fe
natman99 <nat@nat-desktop.home.myserenity.net>
2026-05-26 17:31:24 -0500

    fix command

--- serenity_cli/src/commands/switch.rs
+++ serenity_cli/src/commands/switch.rs
@@ -1,19 +1,7 @@
 use crate::{App, widgets::State};
 
-pub fn spell(command: &str, state: &mut App) -> crate::command::Result<()> {
-    let split = command.split(" ").collect::<Vec<&str>>();
-    let num;
-    if let Some(n) = split.get(1) {
-        if let Ok(n) = n.parse::<usize>() {
-            num = Some(n);
-        } else {
-            return Err(crate::command::CommandError::ArgumentError);
-        }
-    } else {
-        num = None;
-    }
-
-    state.state = State::Spells(num);
+pub fn spell(_command: &str, state: &mut App) -> crate::command::Result<()> {
+    state.state = State::Spells;
 
     Ok(())
 }
